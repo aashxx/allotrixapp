9dd2de080a1f0b70c670c6ddd8b24928dc1740a6
Atheeb <atheebvalued@gmail.com>
2023-09-18 15:44:52 +0530

    hellllllllooo

--- script.js
+++ script.js
@@ -108,6 +108,16 @@ function preOrderScreen(){
   preOrderScreenDiv.classList.add("preOrderScreenDiv")
   document.body.appendChild(preOrderScreenDiv);
 
+
+
+  const backgroundText2 = document.createElement("h1");
+  backgroundText2.classList.add("background-text");
+  backgroundText2.textContent = "allotrix";
+  backgroundText2.style.bottom = "-446px";
+  backgroundText2.style.right = "-161px";
+
+  preOrderScreenDiv.appendChild(backgroundText2);
+
   const backBtnContainer= document.createElement("div");
   backBtnContainer.classList.add("back-btn-container");
 
