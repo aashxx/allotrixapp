2276757dc2d99ef444ce9f5aec89bbc924538e5a
Atheeb <atheebvalued@gmail.com>
2023-11-12 14:53:44 +0530

    changed wc to id

--- renderer.js
+++ renderer.js
@@ -1,25 +1,40 @@
 import {createHeroSection, whyChooseContainer, features, pricing, lifeTimeOffer, footer,
 showLoadingScreen, hideLoadingScreen, createLoadingScreen} from "./index.js"
+const res_nav = document.getElementById('mobile-menu-button');
+const navbar = document.getElementById('navbar');
 
 document.addEventListener("DOMContentLoaded", loadLandingpage)
 
 
 async function loadLandingpage(){
-    showLoadingScreen();
-    try{
-        await delay(2000);
+    //showLoadingScreen();
+   // try{
+        //await delay(2000);
         createHeroSection();
         whyChooseContainer();
         features();
         pricing();
         lifeTimeOffer();
         footer();
-    }
-    finally{
-        hideLoadingScreen()
-    }
+    //}
+    //finally{
+        //hideLoadingScreen()
+    //}
 }
 
 function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+// Responsive Nav
+let state = 1;
+function showNav() {
+  if(state == 1) {
+    navbar.style.width='100%';
+    state = 0;
+  } else {
+    navbar.style.width='0%';
+    state = 1;
+  }
 }
